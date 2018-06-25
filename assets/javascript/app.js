a0b239bbf0e8db8efaa7e81daea2b3bdf5566242
Throwback74 <corey.slade@gmail.com>
2018-06-24 22:34:31 -0700

    in progress, still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,5 @@
-    
+$(document).ready(function() {    
     var topics = ["Pokemon", "Tomagatchi", "Batman", "Playstation", "Jak & Daxter", "Ratchet & Clank", "Zelda", "Animorphs", "Goosebumps", "Dragon Ball Z"];
-
-    var gifPuller = function() {
-
-        // $("button").on("click", function() {
-
-        var topic = $(this).attr("data-target");
-
-        // original example url - "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy.gif",
-
-        //Modified w/ original_still "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy_s.gif",
-        
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=V2140B3NOAnFb0N0CCuJzkaiHaGjwFVH&q=" + topic + "&limit=10&offset=0&lang=en";
-
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=asAO8ArUemSJHtUVSaVcX88Hzgl2FIgn&limit=10";
-
-            $.ajax({
-            url: queryURL,
-            method: "GET"
-            }).then(function(response) {
-                console.log(response);
-        
-                var results = response.data;
-
-                for (var i = 0; i < data.length; i++) {
-
-                    var favoritesDiv = $("<div class='faves'>");
-                
-                    var p = $("<p class ='faveGifs'>");
-
-                    var rating = $(p).html("Rating: " + results[i].rating);
-                    
-                    favoritesDiv.append(rating);
-
-                    var stillURL = results[i].images.original_still;
-                    
-                    var fixedStillURL = results[i].images.fixed_width_still;
-
-                    // var stillImage = $("<img>").attr("src", stillURL);
-                    
-                    var origURL = results[i].images.original;
-
-                    var image = $("<img>");
-
-                    image.addClass("gif");
-
-                    image.attr("data-state", "still");
-
-                    image.attr("src", stillURL);
-
-
-                    // var animatedImage = $("<img>").attr("src", origURL);
-                    // console.log(animatedImage);
-
-                    favoritesDiv.append(image);
-
-                    $("#favoriteGifs").prepend(favoritesDiv);
-
-                };
-            });
-    };
-
-    var pausePlay = $(".gif").click(function() {
-
-        var state = $(this).attr("data-state");
-
-        if(state === "still") {
-            $(this).attr("src", $(this).attr(gifPuller.animatedImage));
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr(gifPuller.stillImage));
-            $(this).attr("data-state", "still");
-        };
-
-    });
 
     // The Function that creates the buttons for each item
     function renderButtons() {
@@ -104,8 +30,110 @@
         renderButtons();
     });
 
-// Adding a click event listener to all elements with a class of "movie-btn"
+    var gifPuller = function() {
+
+        // $("button").on("click", function() {
+
+        var topic = $(this).attr("data-name");
+
+        // original example url - "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy.gif",
+
+        //Modified w/ original_still "http://media0.giphy.com/media/feqkVgjJpYtjy/giphy_s.gif",
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=V2140B3NOAnFb0N0CCuJzkaiHaGjwFVH&q=" + topic + "&limit=10&offset=0&lang=en";
+
+        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=asAO8ArUemSJHtUVSaVcX88Hzgl2FIgn&limit=10";
+
+            $.ajax({
+            url: queryURL,
+            method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                console.log(response.data[0].images.original_still.url);
+                console.log(response.data[i].images.original_still.url);
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    // console.log(response.data[i].images.original.url);
+                    console.log(response.data[i].images.original.url);
+
+                    var favoritesDiv = $("<div>");
+
+                    var rating = results[i].rating;
+                
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // var rating = $(p).html("Rating: " + results[i].rating);
+                    
+                    var topicImage = $("<img>");
+
+                    var origURL = results[i].images.original.url;
+                    console.log("original: " + origURL);
+
+                    var altStillURL = origURL + "_s";
+                    console.log("altstill: " + altStillURL);
+
+                    var stillURL = results[i].images.original_still.url;
+                    console.log("still: " + stillURL);
+
+                    topicImage.attr({
+                        src: stillURL,
+                        dataState: "still"
+                    });
+
+                    topicImage.addClass("gif");
+
+                    // $(".gif").attr("dataState", "still");
+
+                    favoritesDiv.append(p);
+
+                    favoritesDiv.append(topicImage);
+
+                    $("#favoriteGifs").prepend(topicImage);
+
+                };
+            });
+    };
+                    // var fixedStillURL = results[i].images.fixed_width_still;
+
+                    // var stillImage = $("<img>").attr("src", stillURL);
+                    
+                    // var origURL = results[i].images.original.url;
+
+                    // var image = $("<img>").attr("src", stillURL);
+
+                    // image.addClass("gif");
+
+                    // image.attr("dataState", "still");
+
+                    // favoritesDiv.append(p);
+
+                    // image.attr("src", stillURL);
+
+                    // var animatedImage = $("<img>").attr("src", origURL);
+                    // console.log(animatedImage); 
+                    // $("#favoriteGifs").append(favoritesDiv);
+                    
+                    // favoritesDiv.append(image);
+
+//var pausePlay = 
+    $(".gif").click(function() {
+
+        var state = $(this).attr("dataState");
+
+        if(state === "still") {
+            $(this).attr("src", $(this).attr(gifPuller.origURL));
+            $(this).attr("dataState", "animate");
+        } else {
+            $(this).attr("src", $(this).attr(gifPuller.stillURL));
+            $(this).attr("dataState", "still");
+        };
+
+    });
+
+// Adding a click event listener to all elements with a class of favorites-btn
 $(document).on("click", ".favorites-btn", gifPuller);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();      
+});
